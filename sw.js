601452ff4fc1bc17636c0a01a50903b80140c7bc
namgeunho <namgeunho@gmail.com>
2026-04-15 00:45:36 +0900

    Update sw.js
    1-1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'breath-v3';
+const CACHE = 'brethin-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -23,14 +23,25 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached =>
-      cached || fetch(e.request).then(res => {
-        if (res.ok && e.request.url.startsWith('http')) {
-          caches.open(CACHE).then(c => c.put(e.request, res.clone()));
-        }
+  // index.html은 항상 네트워크 우선 (최신 버전 보장)
+  if (e.request.url.includes('index.html') || e.request.url.endsWith('/')) {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
         return res;
-      }).catch(() => cached)
-    )
+      }).catch(() => caches.match(e.request))
+    );
+    return;
+  }
+  // 나머지는 캐시 우선
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      if (res.ok && e.request.url.startsWith('http')) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }))
   );
 });
